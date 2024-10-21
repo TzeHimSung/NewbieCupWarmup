a77bd6b43d1e6e65e08229913a10c3f1dee263d3
wty25 <112966779+wty25@users.noreply.github.com>
2024-10-22 02:09:20 +0800

    Update std.cpp

--- E/std.cpp
+++ E/std.cpp
@@ -1,23 +1,28 @@
 #include<bits/stdc++.h>
 #define ll long long
 using namespace std;
+#define ull unsigned long long
+
 ll n;
+
 void solve()
 {
 	cin>>n;
-	if(n==1)cout<<"7 6\n";
-	if(n==2)cout<<"11 9\n";
-	if(n==3)cout<<"7 4\n";
+	ll a,b;
+	if(n==1)a=7,b=6;
+	if(n==2)a=11,b=9;
+	if(n==3)a=7,b=4;
 	if(n>3)
 	{
-		if(n%2)cout<<3<<" "<<(n^(ll)3)<<"\n";
-		else cout<<2<<" "<<(n^(ll)2)<<"\n";
+		if(n%2)a=3,b=n^(ll)3;
+		else a=2,b=n^(ll)2;
 	}
-	
+	cout<<a<<" "<<b<<"\n";
 }
 int main(){
 	ios::sync_with_stdio(false);
 	cin.tie(0),cout.tie(0);
+//	freopen("D:\\1.in","r",stdin); //输入重定向，输入数据将从D盘根目录下的in.txt文件中读取 
 	int T=1;
 	cin>>T;
 	while(T--) solve();
